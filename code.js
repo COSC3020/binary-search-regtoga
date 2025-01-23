b0bc98d84f9e64c5bf7589a53af87444c2e1fc3b
Aidan Newberry <91752566+regtoga@users.noreply.github.com>
2025-01-23 13:09:46 -0700

    made it more readable

--- code.js
+++ code.js
@@ -1,9 +1,7 @@
 const { int32 } = require("jsverify");
 
 function binarySearch(list, element) {
-    //list.sort((a, b) => a - b);
-    //console.log("inputs: (" + list + ") " + element)
-    //console.log("list thing: " + list[list.length-1] + ", element: " + element)
+    //not going to lie these are hardcoded fixes to some small issues
     if (!Array.isArray(list)) {
         return -1;
     }else if ((list[list.length-1] < element)) {
@@ -14,17 +12,17 @@ function binarySearch(list, element) {
         return 0;
     }
 
+    //set up the variables for further computation
     var length = list.length - 1;
     const decimalPart = length/2 - Math.floor(length/2);
     var middle = Math.round(length/2);
+
+    //if we had to round add a one to the index to fix the fact that there isnt always a way to divide the array in half nicely.
     if (decimalPart === 0.5) {
         var length_middle = length - middle + 1;
     }else {
         var length_middle = length - middle;
     }
-    
-    //console.log(middle);
-    //console.log("looking for " + element + ", list: (" + list + "), chop at " + middle);
     
     //check if the the element is larger than the middle of the list
     //if so search for it on the left of the middle
@@ -42,16 +40,6 @@ function binarySearch(list, element) {
         return middle;
     }
     
+    //if all fails return false
     return -1;
 }
-
-
-//console.log(binarySearch([1,2,3,4,5,6,7,8,9,10], 4));
-//console.log(binarySearch([1,2,3,4,5,8,9,10], 10));
-//console.log(binarySearch([1,2,3,4,5,8,9,10], 11));
-//console.log(binarySearch([1,9,10], 9));
-//console.log(binarySearch([0, 0], 0));
-//console.log(binarySearch([7,7,7], 7));
-//console.log(binarySearch([4,3], 4));
-//console.log(binarySearch([3,4], 4));
-//console.log(binarySearch([1,0], 1));
