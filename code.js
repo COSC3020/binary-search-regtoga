95c0d0608f97d85cf5e533dadccdcaf19de9efb8
Aidan Newberry <91752566+regtoga@users.noreply.github.com>
2025-02-02 15:01:39 -0700

    I think i addressed your concerns, i also got rid of my jankey hardcoded bs so now its just raw algorithm :)

--- code.js
+++ code.js
@@ -1,45 +1,29 @@
 const { int32 } = require("jsverify");
-
-function binarySearch(list, element) {
-    //not going to lie these are hardcoded fixes to some small issues
-    if (!Array.isArray(list)) {
+//I added some default values so that i can keep track of where we are in the array,
+//this makes it possible to look ahead and find if we are returning the smallest possible value for our index
+//there were probbly other ways to do this but this seemd pretty easy to implement.
+function binarySearch(list, element, left = 0, right = list.length - 1) {
+    //base case when the search goes out of bounds
+    if (left > right) {
         return -1;
-    }else if ((list[list.length-1] < element)) {
-        return -1;
-    }else if (list.length == 2 && list[0] == element) {
-        return 0;
-    }else if (list.length == 3 && list[0] == element) {
-        return 0;
     }
 
-    //set up the variables for further computation
-    var length = list.length - 1;
-    const decimalPart = length/2 - Math.floor(length/2);
-    var middle = Math.round(length/2);
+    //find the middle
+    var middle = Math.floor((left + right) / 2);
 
-    //if we had to round add a one to the index to fix the fact that there isnt always a way to divide the array in half nicely.
-    if (decimalPart === 0.5) {
-        var length_middle = length - middle + 1;
-    }else {
-        var length_middle = length - middle;
-    }
-    
-    //check if the the element is larger than the middle of the list
-    //if so search for it on the left of the middle
-    if (element < list[middle]){
-        return binarySearch(list.slice(0, middle), element);
-    }
-
-    //else search for it on the right
-    else if (element > list[middle]){
-        return length_middle + binarySearch(list.slice(middle), element)
-    }
-
-    //if the element IS the middle index then we found our boi
-    else if (element == list[middle]){
+    if (list[middle] == element) {
+        // Check left of where we are currently to see if there is a more minimal value
+        while (middle > left && list[middle - 1] == element) {
+            middle -= 1;
+        }
         return middle;
+    } else if (element < list[middle]) {
+        //if is on the left
+        return binarySearch(list, element, left, middle - 1);
+    } else {
+        //if is on the right
+        return binarySearch(list, element, middle + 1, right);
     }
-    
-    //if all fails return false
-    return -1;
 }
+
+//console.log(binarySearch([1,2,3,3,4,5,6,7,8,9,1515], 3));
